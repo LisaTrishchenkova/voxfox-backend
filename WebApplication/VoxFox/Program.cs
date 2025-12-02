c9f49ad731ee839414467099e81c0d0e2c018d40
Alexander Gulidov <ecernenko090@gmail.com>
2025-12-02 16:26:10 +0300

    init project add dockercompose

--- WebApplication/VoxFox/Program.cs
+++ WebApplication/VoxFox/Program.cs
@@ -10,18 +10,20 @@ namespace VoxFox
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwaggerUI();
+                app.UseSwagger();
+
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
