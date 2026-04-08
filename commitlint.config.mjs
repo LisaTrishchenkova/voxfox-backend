709def489b9c35273419f400337145aa950961f8
Alexander Gulidov <derideveloper@gmail.com>
2026-04-08 14:47:54 +0300

    fix: resolve dev environment configuration

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,7 +1,28 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [0],
+    "header-max-length": [2, "always", 200],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "ci",
+        "build",
+        "revert",
+      ],
+    ],
+  },
   ignores: [
-    (message) => /^Merge/.test(message),
-    (message) => /\(#\d+\)$/.test(message),
+    (message) => /^Merge pull request/.test(message),
+    (message) => message.includes("squash"),
   ],
 };
